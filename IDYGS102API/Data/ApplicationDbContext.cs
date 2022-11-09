e7e6968f5851c9f5041cd52ae8d0704facbf3a7c
MARIA JOSE SOSA <majo9722@outlook.com>
2022-11-08 19:01:15 -0600

    Agregar archivos de proyecto.

---
+++ IDYGS102API/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Domain.Modelo;
+using Microsoft.EntityFrameworkCore;
+
+namespace IDYGS102API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options) { }
+
+        public DbSet<Personaje> personajes { get; set; }
+        public DbSet<Genero> generos { get; set; }   
+
+    }
+}
